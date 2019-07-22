803572cf974de78aafa76152cbfa76ead9f87771
Sahil Patel <sahil.patel@verizon.com>
2019-07-22 14:20:37 -0400

    Iptaker02

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+user_input = input('Input an IPv4 address: ')
+print(f'The input is {user_input}')
+
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_input = input('Give me an ipv4 address')
+print("Input: ", user_input)
+
+vendor_name = input('Vendor name? ')
+print(f'Name input: {vendor_name}')
