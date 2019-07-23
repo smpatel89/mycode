831ebc48d39b03cf92c891d4b9655d0458b28aef
Sahil Patel <sahil.patel@verizon.com>
2019-07-23 15:12:14 -0400

    If statements

---
+++ if-test/condition02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+hostname = input("What value should be set for hostname? ")
+if hostname == "MTG":
+    print("The hostname was found to be MTG")
---
+++ if-test/condition03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+hostname = input("What should the hostname be set to? ")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be MTG")
+    print("The hostname matches expected config")
+
+print("Exiting script")
+
