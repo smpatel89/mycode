9b5a829313495341f36d0989595dc0152c256bf5
smpatel89 <sahil.patel@verizon.com>
2019-07-24 11:05:11 -0400

    looping01

--- copycat01.py
+++ copycat01.py
@@ -3,5 +3,6 @@ import shutil
 import os
 
 os.chdir('/home/student/mycode/')
+
 shutil.copy('5g_research/sdn_network.txt','5g_research/sdn_network.txt.copy')
 shutil.copytree('5g_research/', '5g_research_backup')
